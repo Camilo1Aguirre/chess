da85d650d3a861c51e764413c49ff674f40e7c7e
Camilo1Aguirre <bcamilo.aguirre@udea.edu.co>
2024-08-01 23:08:56 -0500

    Agregue el boton de reiniciar juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Game } from './components/Game';
-import { Header } from './components/Header';
-import { Turn } from './components/Turn';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header />
       <Game />
-      <Turn />
     </>
   );
 }
